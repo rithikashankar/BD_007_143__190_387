1558cee9085c802f631646dd99b9b8bbc791a9cb
ishita1864 <62196026+ishita1864@users.noreply.github.com>
2021-11-12 22:43:19 +0530

    Add files via upload

--- read.py
+++ read.py
@@ -3,46 +3,49 @@ import re
 
 from pyspark import SparkContext
 from pyspark.sql.context import SQLContext
-import spark.implicits._
 from pyspark.sql import Row
 from pyspark.streaming import StreamingContext
+from pyspark.mllib.clustering import KMeans, KMeansModel, StreamingKMeans
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql.functions import udf
 import operator
-# if __name__ == "__main__":
-#     conf = SparkConf().setAppName("Listening to a tcp port")
-#     sc   = SparkContext(conf=conf)
-#     ssc  = StreamingContext(sc, 1)
+if __name__ == "__main__":
+     
+     sc   = SparkContext(appName='test')
+     ssc  = StreamingContext(sc, 1)
 
-#     jsonn = ssc.socketTextStream("localhost", 6100)
-#     jsonn.pprint(1)
-#     r=jsonn.flatMap(lambda x:x.split('\n'))
-#     r1=r.map(lambda x:x.strip().split(',',1))
-
+     jsonn = ssc.socketTextStream("localhost", 6100)
+#    jsonn.pprint(1)
+     r=jsonn.flatMap(lambda x:x.split(','))
+#    r1=r.map(lambda x:x.strip().split(',',1))
+     r.pprint()
 
     
 
-#     ssc.start()             # Start the computation
-#     ssc.awaitTermination()  # Wait for the computation to terminate
+     ssc.start()             # Start the computation
+     ssc.awaitTermination()  # Wait for the computation to terminate
 
-sc = SparkContext(appName="test")
-ssc = StreamingContext(sc, 1)
-sqlContext = SQLContext(sc)
 
-def streamrdd_to_df(srdd):
-    sdf = sqlContext.createDataFrame(srdd)
-    sdf.show(n=2, truncate=False)
-    return sdf
+#sc = SparkContext(appName="test")
+#ssc = StreamingContext(sc, 1)
+#sqlContext = SQLContext(sc)
 
-def main():
-    indata = ssc.socketTextStream('localhost',6100)
-    inrdd = indata.map(lambda r: tuple(r))
-    df=inrdd.toDF()
-    df.show()
+#def streamrdd_to_df(srdd):
+#    sdf = sqlContext.createDataFrame(srdd)
+#    sdf.show(n=2, truncate=False)
+ #   return sdf
 
-    ssc.start()
-    ssc.awaitTermination()
+#def main():
+ #   indata = ssc.socketTextStream('localhost',6100)
+#    inrdd = indata.map(lambda r: tuple(r))
+  #  df=inrdd.toDF()
+   # df.show()
 
-if __name__ == "__main__":
-    main()
+  # ssc.start()
+    #ssc.awaitTermination()
+
+#if __name__ == "__main__":
+ #   main()
+
+
