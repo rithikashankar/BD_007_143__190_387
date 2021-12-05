a9e0474c730f1ff66b24650cf38cd3160ab652f1
ishita1864 <62196026+ishita1864@users.noreply.github.com>
2021-12-06 00:31:21 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -15,6 +15,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn import metrics
+
+
+from sklearn.cluster import MiniBatchKMeans, KMeans
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+
   
 # creating sparksession and giving an app name
 from sklearn.linear_model import SGDClassifier
@@ -50,7 +56,7 @@ def preprocess(data):
 			l=(j[i]['feature0'],j[i]['feature1'])
 			rows.append(l)
 		df=spark.createDataFrame(rows,col)
-          # df.printSchema()
+		df.printSchema()
 
           #cleaning the data
 		df = df.na.replace('', None)
@@ -91,12 +97,38 @@ def preprocess(data):
 		print(end-start)
 
 
+		# t0 = time.time()
+		# mbk.fit_transform(X_train)
+		# t_mini_batch = time.time() - t0
+		# mbk_means_labels = mbk.labels_
+		# mbk_means_cluster_centers = mbk.cluster_centers_
+		# mbk_means_labels_unique = np.unique(mbk_means_labels)
+		# print('ddededede')
+
+		print("Clustering sparse data with %s" % km)
+		t0 = time.time()
+		km.fit_transform(X_train)
+		print("done in %0.3fs" % (time.time() - t0))
+		print('\n')
+
+		labels= df['label']
+		#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
+		#print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
+		
+		# print('hi ish\n\n\n')
+
+
+
 
 
 if __name__ == "__main__":
 	bnb=BernoulliNB(alpha=0.5,fit_prior=True)
 	mnb=MultinomialNB(alpha=0.5,fit_prior=True)
 	sgd=SGDClassifier(loss='log')
+	#mbk = MiniBatchKMeans(init='k-means++', n_clusters=3, batch_size=6, n_init=10, max_no_improvement=10, verbose=0)
+	km = MiniBatchKMeans(n_clusters=3, init='k-means++', n_init=1,
+                         init_size=1000, batch_size=1000, verbose=0)
+	
 	vectorizer = HashingVectorizer(
     decode_error="ignore", n_features=100000, alternate_sign=False)
 	sc   = SparkContext(appName='test')
