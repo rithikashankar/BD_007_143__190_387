0610f290f8a0acfcb23783d43d17c81162ca5251
abdulmk787 <55476136+abdulmk787@users.noreply.github.com>
2021-12-06 16:00:04 +0530

    Update train.py

--- train.py
+++ train.py
@@ -15,12 +15,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
-from sklearn import metrics
-
-
-from sklearn.cluster import MiniBatchKMeans, KMeans
-from sklearn.metrics.pairwise import pairwise_distances_argmin
-
   
 # creating sparksession and giving an app name
 from sklearn.linear_model import SGDClassifier
@@ -56,7 +50,7 @@ def preprocess(data):
 			l=(j[i]['feature0'],j[i]['feature1'])
 			rows.append(l)
 		df=spark.createDataFrame(rows,col)
-		df.printSchema()
+          # df.printSchema()
 
           #cleaning the data
 		df = df.na.replace('', None)
@@ -83,39 +77,25 @@ def preprocess(data):
 		y_train=dnp[:,0]
 		
 		X_train=vectorizer.fit_transform(X_train)
-
+		end=time.time()
+		print(end-start,end=',')
 		c=np.unique(y_train)
-		
+		start=time.time()
 		mnb.partial_fit(X_train,y_train,classes=c)
+		end=time.time()
+		print(end-start,end=',')
+		start=time.time()
 		sgd.partial_fit(X_train,y_train,classes=c)
+		end=time.time()
+		print(end-start,end=',')
+		start=time.time()
 		bnb.partial_fit(X_train,y_train,classes=c)
+		end=time.time()
+		print(end-start)
 		pickle.dump(mnb,open('mnb.sav','wb'))
 		pickle.dump(bnb,open('bnb.sav','wb'))
 		pickle.dump(sgd,open('sgd.sav','wb'))
 		pickle.dump(vectorizer,open('vector.pk','wb'))
-		end=time.time()
-		print(end-start)
-
-
-		# t0 = time.time()
-		# mbk.fit_transform(X_train)
-		# t_mini_batch = time.time() - t0
-		# mbk_means_labels = mbk.labels_
-		# mbk_means_cluster_centers = mbk.cluster_centers_
-		# mbk_means_labels_unique = np.unique(mbk_means_labels)
-		# print('ddededede')
-
-		print("Clustering sparse data with %s" % km)
-		t0 = time.time()
-		km.fit_transform(X_train)
-		print("done in %0.3fs" % (time.time() - t0))
-		print('\n')
-
-		labels= df['label']
-		#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
-		#print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
-		
-		# print('hi ish\n\n\n')
 
 
 
@@ -125,19 +105,16 @@ if __name__ == "__main__":
 	bnb=BernoulliNB(alpha=0.5,fit_prior=True)
 	mnb=MultinomialNB(alpha=0.5,fit_prior=True)
 	sgd=SGDClassifier(loss='log')
-	#mbk = MiniBatchKMeans(init='k-means++', n_clusters=2, batch_size=6, n_init=10, max_no_improvement=10, verbose=0)
-	km = MiniBatchKMeans(n_clusters=2, init='k-means++', n_init=1,
-                         init_size=1000, batch_size=1000, verbose=0)
-	
 	vectorizer = HashingVectorizer(
     decode_error="ignore", n_features=100000, alternate_sign=False)
 	sc   = SparkContext(appName='test')
 	spark = SparkSession.builder.appName('sparkdf').getOrCreate()
-	ssc  = StreamingContext(sc, 5)
+	ssc  = StreamingContext(sc, 2)
 	sqlContext = SQLContext(sc)
 	lines = ssc.socketTextStream("localhost", 6100)
 	words=lines.flatMap(lambda line: line.split('\n'))
 	lines.foreachRDD(preprocess)
+	print('vector,mnb,sgd,bnb')
 	ssc.start()             # Start the computation
 	
 	ssc.awaitTermination()  # Wait for the computation to terminate
